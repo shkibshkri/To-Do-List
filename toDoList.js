6e90ec73b758198389588efc127ad74ee6eeb492
f-shakeri <shkibshkri@gmail.com>
2023-08-09 13:43:32 +0330

    last commit

--- toDoList.js
+++ toDoList.js
@@ -1,43 +1,36 @@
-function addRow()
+function inputTask()
+{
+    let task = document.getElementById("task").value;
+    if(task.length > 16)
+    {
+        if(confirm("Warning: Your task is too long. It might not work well!") === true)
+        {
+            addRow(task);
+        } else 
+        {   
+            document.getElementById("task").value = "";
+        }
+    } else if(task.length <= 16)
+    {
+        addRow(task);
+    }
+}
+
+function addRow(item)
 {
     let table = document.getElementById("table");
     let newRow = table.insertRow(1);
     let newCell = newRow.insertCell(0);
     newCell.colSpan = "2";
 
-    let item = inputTask();
-
-    if(item.length > 16)
-    {
-        if(confirm("Warning: Your task is too long. It might not work well!") === true)
-        {
-            newCellDetail(newCell, item);
-        } else 
-        {   
-            document.getElementById("task").value = "";
-        }
-    } else if(item.length <= 16)
-    {
-        newCellDetail(newCell, item);
-    }
-} 
-
-function inputTask()
-{
-    let task = document.getElementById("task").value;
-    return task;
-}
-
-function newCellDetail(newCell, item)
-{
     newCell.innerHTML = `<input type="checkbox" class="checkboxStyle" onclick="lineThrough(this)">
                             <img src="images/trash.svg" class="trashStyle" onclick="deleteTask(this)">
                             <img src="images/pencil-svgrepo-com.svg" class="pencilStyle" onclick="editTask(this)">
                             <div>${item}</div>
                         </input>`                 
-    
+
     document.getElementById("task").value = "";
-}
+} 
 
 function lineThrough(line)
 {
